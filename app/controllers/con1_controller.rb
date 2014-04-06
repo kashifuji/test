f9962c2ca64e5cc5f4505b96b94f24cd3c7deb47
kashifuji <katashiyo515@yahoo.co.jp>
2014-04-06 13:39:10 +0900

    modified con1 controller

--- app/controllers/con1_controller.rb
+++ app/controllers/con1_controller.rb
@@ -1,7 +1,11 @@
 class Con1Controller < ApplicationController
   def index
     u = User.find(1)
-    @name = u.name
+    if u.name = nil
+      @name = "guest"
+    else
+      @name = u.name
+    end
   end
   def error
     render :nothing => true, :status => 404
